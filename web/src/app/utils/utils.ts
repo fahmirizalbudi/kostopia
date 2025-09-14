e43d9faeb6b199087a2913012813be86dbf05dd1
fahmirizalbudi <fahmirizal.dev@gmail.com>
2025-09-14 11:00:46 +0700

    fix: fix conditional to render an option

--- web/src/app/utils/utils.ts
+++ web/src/app/utils/utils.ts
@@ -1,7 +1,7 @@
 import { Option } from "../components/forms/ComboBox"
 
 export function findOption(options: Option[], value: string | undefined | null): Option | null {
-  return options.find((option) => option.value === value) || null
+  return options?.find((option) => option.value === value) || null
 }
 
 export function asc(arr: any[], by: string): any[] {
