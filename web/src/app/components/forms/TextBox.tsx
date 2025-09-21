98b98239902b8624134813e53b1e43f6211c54cf
fahmirizalbudi <fahmirizal.dev@gmail.com>
2025-09-21 07:06:39 +0700

    feat: new textbox with optional icon

--- web/src/app/components/forms/TextBox.tsx
+++ web/src/app/components/forms/TextBox.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import styles from "./TextBox.module.scss"
 
 type TextBoxProps = {
@@ -7,10 +8,15 @@ type TextBoxProps = {
   name?: string
   value?: number | string
   className?: string
+  icon?: string
+  iconSize?: number
 }
 
-const TextBox = ({ type, placeholder, onChange, name, value, className }: TextBoxProps) => (
-  <input className={`${styles.textbox} ${className}`} type={type} placeholder={placeholder} onChange={onChange} name={name} value={value} id={name} autoComplete="off" />
+const TextBox = ({ type, placeholder, onChange, name, value, className, icon, iconSize }: TextBoxProps) => (
+  <div className={`${styles.textbox} ${className}`}>
+    {icon && <Image src={icon} width={iconSize} height={iconSize} alt="Icon" />}
+    <input type={type} placeholder={placeholder} onChange={onChange} name={name} value={value} id={name} autoComplete="off" />
+  </div>
 )
 
 export default TextBox
