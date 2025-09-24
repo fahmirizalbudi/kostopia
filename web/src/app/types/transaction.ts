ce4c0b5d146ef75080cc30e18f4e6b84ba4273cf
fahmirizalbudi <fahmirizal.dev@gmail.com>
2025-09-25 03:20:11 +0700

    feat: create transaction type

---
+++ web/src/app/types/transaction.ts
@@ -0,0 +1,12 @@
+export type Transaction = {
+  id?: string
+  rental_id?: number
+  dormitory_price?: number
+  month_paid?: number
+  amount?: number
+  method?: string
+  purpose?: string
+  status?: string
+  proof?: string
+  created_at?: string
+}
