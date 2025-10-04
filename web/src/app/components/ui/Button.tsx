87ee2deb6954366938f9799ca134443884d1494d
fahmirizalbudi <fahmirizal.dev@gmail.com>
2025-10-04 18:07:51 +0700

    fix: fix undefined selected on rent

--- web/src/app/components/ui/Button.tsx
+++ web/src/app/components/ui/Button.tsx
@@ -3,11 +3,12 @@ type ButtonProps = {
     onClick?: () => void,
     children: React.ReactNode,
     type?: "button" | "submit"
+    disabled?: boolean
 }
 
 const Button = (buttonProps: ButtonProps) => {
   return (
-    <button className={buttonProps.className} onClick={buttonProps.onClick} type={buttonProps.type}>{buttonProps.children}</button>
+    <button className={buttonProps.className} onClick={buttonProps.onClick} type={buttonProps.type} disabled={buttonProps.disabled}>{buttonProps.children}</button>
   )
 }
 
