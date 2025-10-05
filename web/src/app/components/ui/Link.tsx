9c2d95ab2492344ba769be4496bf0e662488e324
fahmirizalbudi <fahmirizal.dev@gmail.com>
2025-10-05 17:28:59 +0700

    fix: fix link ui

--- web/src/app/components/ui/Link.tsx
+++ web/src/app/components/ui/Link.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import NextLink, { LinkProps } from "next/link"
+import { useRouter } from "next/navigation"
 import NProgress from "nprogress"
 import { MouseEvent } from "react"
 
@@ -9,14 +10,23 @@ type Props = LinkProps & {
   className?: string
 }
 
-export default function Link({ children, onClick, ...props }: Props) {
-  const handleClick = (e: MouseEvent<HTMLAnchorElement>) => {
+export default function Link({ children, onClick, href, ...props }: Props) {
+  const router = useRouter()
+
+  const handleClick = async (e: MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault()
     NProgress.start()
+    if (href) {
+      router.push(href as string)
+    }
+    setTimeout(() => {
+      NProgress.done()
+    }, 1000)
     onClick?.(e)
   }
 
   return (
-    <NextLink {...props} onClick={handleClick}>
+    <NextLink {...props} href={href} onClick={handleClick}>
       {children}
     </NextLink>
   )
