a077b4de0426d91f12ca627d83085dc2ba8fbc8e
fahmirizalbudi <fahmirizal.dev@gmail.com>
2025-10-05 17:03:48 +0700

    feat: operate the filter on dormitory

--- web/src/app/dormitories/components/FilterDormitory.tsx
+++ web/src/app/dormitories/components/FilterDormitory.tsx
@@ -6,24 +6,82 @@ import TextBox from "@/app/components/forms/TextBox"
 import Flex from "@/app/components/layout/Flex"
 import { asset } from "@/app/lib/asset"
 import styles from "../page.module.scss"
+import { useState } from "react"
+import { useRouter, useSearchParams } from "next/navigation"
+import nProgress from "nprogress"
+import { findOption } from "@/app/utils/utils"
 
-const FilterDormitory = () => {
-  const prices: Option[] = [{ value: "300000 - 500000", label: "Rp350.000 - Rp500.000" }]
+type FilterDormitoryProps = {
+  facilities: Option[]
+  prices: Option[]
+}
 
-  const facilities: Option[] = [
-    { value: "ac", label: "AC" },
-    { value: "wifi", label: "Wi-Fi" },
-    { value: "parkir", label: "Parkir" },
+const FilterDormitory = ({ facilities, prices }: FilterDormitoryProps) => {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const [keywords, setKeywords] = useState<string>(searchParams.get("keywords") || "")
+  const [price, setPrice] = useState<string>(searchParams.get("price") || "")
+  const [facility, setFacility] = useState<string>(searchParams.get("facility") || "")
+  const [order, setOrder] = useState<string>(searchParams.get("order") || "")
+  const orders: Option[] = [
+    { value: "price_asc", label: "Harga: Terendah ke Tertinggi" },
+    { value: "price_desc", label: "Harga: Tertinggi ke Terendah" },
+    { value: "name_asc", label: "Nama: A-Z" },
+    { value: "name_desc", label: "Nama: Z-A" },
   ]
+
+  const handleSearch = () => {
+    nProgress.start()
+
+    const params = new URLSearchParams()
+    if (keywords) params.set("keywords", keywords)
+    if (price) params.set("price", price)
+    if (facility) params.set("facility", facility)
+    if (order) params.set("order", order)
+
+    setTimeout(() => {
+      router.replace(`/dormitories?${params.toString()}`)
+      setTimeout(() => {
+        nProgress.done()
+      }, 500)
+    }, 1000)
+  }
+
+  const handleRefresh = () => {
+    nProgress.start()
+    setKeywords("")
+    setPrice("")
+    setFacility("")
+    setOrder("")
+    setTimeout(() => {
+      router.replace(`/dormitories`)
+      setTimeout(() => {
+        nProgress.done()
+      }, 500)
+    }, 1000)
+  }
 
   return (
     <Flex className={styles.filterContainer}>
-      <TextBox name="search" type="text" placeholder="Masukkan kata kunci pencarian ..." className={styles.search} />
-      <ComboBox options={prices} placeholder="Urutkan berdasarkan" className={styles.orderBy} />
-      <ComboBox options={prices} placeholder="Rentang harga" />
-      <ComboBox options={facilities} placeholder="Fasilitas" />
-      <Action icon={asset("reload.svg")} size={18} as="Refresh" className={styles.action} />
-      <Action icon={asset("search_02.svg")} size={19} as="Search" className={styles.action} />
+      <TextBox
+        name="search"
+        type="text"
+        placeholder="Masukkan kata kunci pencarian ..."
+        className={styles.search}
+        value={keywords}
+        onChange={(e) => setKeywords(e.target.value)}
+      />
+      <ComboBox
+        options={orders}
+        value={findOption(orders, order)}
+        onChange={(option) => setOrder(option)}
+        placeholder="Urutkan berdasarkan"
+        className={styles.orderBy}
+      />
+      <ComboBox options={prices} value={findOption(prices, price)} onChange={(option) => setPrice(option)} placeholder="Rentang harga" />
+      <ComboBox options={facilities} placeholder="Fasilitas" value={findOption(facilities, facility)} onChange={(option) => setFacility(option)} />
+      <Action icon={asset("reload.svg")} size={18} as="Refresh" className={styles.action} onAction={handleRefresh} />
+      <Action icon={asset("search_02.svg")} size={19} as="Search" className={styles.action} onAction={handleSearch} />
     </Flex>
   )
 }
--- web/src/app/dormitories/page.tsx
+++ web/src/app/dormitories/page.tsx
@@ -8,20 +8,100 @@ import styles from "./page.module.scss"
 import Grid from "../components/layout/Grid"
 import FilterDormitory from "./components/FilterDormitory"
 import { fetchDormitoriesWithPreviews } from "../data-access/dormitories"
-import { asc } from "../utils/utils"
-import { FIELD_ID } from "../constants/field"
 import { Dormitory } from "../types/dormitory"
 import DormitoryCard from "./components/DormitoryCard"
 import DormitoryList from "./components/DormitoryList"
+import { Option } from "../components/forms/ComboBox"
+import Link from "next/link"
 
 type DormitoriesProps = {
   searchParams?: {
     type?: string
+    keywords?: string
+    facility?: string
+    price?: string
+    order?: string
   }
 }
 
 const Dormitories = async ({ searchParams }: DormitoriesProps) => {
   const dormitoriesWithPreviews = await fetchDormitoriesWithPreviews()
+  let filteredDormitoriesWithPreviews = dormitoriesWithPreviews
+
+  if (searchParams?.keywords) {
+    const keywordLower = searchParams.keywords.toLowerCase()
+    filteredDormitoriesWithPreviews = filteredDormitoriesWithPreviews.filter((dormitory: Dormitory) =>
+      dormitory?.name?.toLowerCase().includes(keywordLower)
+    )
+  }
+  if (searchParams?.facility) {
+    const facilityLower = searchParams.facility.toLowerCase()
+    filteredDormitoriesWithPreviews = filteredDormitoriesWithPreviews.filter((dormitory: Dormitory) =>
+      dormitory?.facilities?.toLowerCase().includes(facilityLower)
+    )
+  }
+
+  if (searchParams?.price) {
+    const [minPriceStr, maxPriceStr] = searchParams?.price.split("|")
+    const minPrice = Number(minPriceStr)
+    const maxPrice = Number(maxPriceStr)
+
+    filteredDormitoriesWithPreviews = filteredDormitoriesWithPreviews.filter((dormitory: Dormitory) => {
+      return Number(dormitory.price) >= minPrice && Number(dormitory.price) <= maxPrice
+    })
+  }
+
+  if (searchParams?.order) {
+    switch (searchParams?.order) {
+      case "price_asc":
+        filteredDormitoriesWithPreviews = filteredDormitoriesWithPreviews.sort(
+          (current: Dormitory, next: Dormitory) => Number(current.price) - Number(next.price)
+        )
+        break
+      case "price_desc":
+        filteredDormitoriesWithPreviews = filteredDormitoriesWithPreviews.sort(
+          (current: Dormitory, next: Dormitory) => Number(next.price) - Number(current.price)
+        )
+        break
+      case "name_asc":
+        filteredDormitoriesWithPreviews = filteredDormitoriesWithPreviews.sort((current: Dormitory, next: Dormitory) =>
+          current.name?.localeCompare(next.name as string)
+        )
+        break
+      case "name_desc":
+        filteredDormitoriesWithPreviews = filteredDormitoriesWithPreviews.sort((current: Dormitory, next: Dormitory) =>
+          next.name?.localeCompare(current.name as string)
+        )
+        break
+      default:
+        break
+    }
+  }
+
+  let rawFacilities: string[] = []
+  for (const dormitory of dormitoriesWithPreviews) {
+    const facilities = dormitory.facilities.split(",")
+    for (const facility of facilities) {
+      rawFacilities.push(facility)
+    }
+  }
+
+  const arrayFacilities = Array.from(new Set(rawFacilities))
+
+  const facilities: Option[] = arrayFacilities.map((facility) => ({
+    value: facility.toLowerCase(),
+    label: facility.charAt(0).toUpperCase() + facility.slice(1),
+  }))
+
+  let prices: Option[] = []
+  const rawPrices = dormitoriesWithPreviews.map((dormitory: Dormitory) => dormitory.price)
+  const minPrice = Math.min(...rawPrices)
+  const maxPrice = Math.max(...rawPrices)
+  const midPrice = Math.floor((minPrice + maxPrice) / 2)
+  prices = [
+    { value: `${minPrice}|${midPrice}`, label: `${minPrice.toLocaleString()} - ${midPrice.toLocaleString()}` },
+    { value: `${midPrice + 1}|${maxPrice}`, label: `${(midPrice + 1).toLocaleString()} - ${maxPrice.toLocaleString()}` },
+  ]
 
   return (
     <main>
@@ -30,23 +110,39 @@ const Dormitories = async ({ searchParams }: DormitoriesProps) => {
         <Flex className={styles.header}>
           <span className={styles.titleSection}>Cari Kos</span>
           <Flex className={styles.decorations}>
-            <Image alt="Decoration" width={40} height={40} src={asset("deco_02.svg")} className={styles.decoration} />
-            <Image alt="Decoration" width={40} height={40} src={asset("deco.svg")} className={styles.decoration} />
+            <Link href="/dormitories?type=list">
+              <Image
+                alt="Decoration"
+                width={40}
+                height={40}
+                src={asset(searchParams?.type === "list" ? "deco_02_active.svg" : "deco_02.svg")}
+                className={styles.decoration}
+              />
+            </Link>
+            <Link href="/dormitories">
+              <Image
+                alt="Decoration"
+                width={40}
+                height={40}
+                src={asset(searchParams?.type === undefined ? "deco_active.svg" : "deco.svg")}
+                className={styles.decoration}
+              />
+            </Link>
           </Flex>
         </Flex>
         <Break height={30} />
-        <FilterDormitory />
+        <FilterDormitory facilities={facilities} prices={prices} />
         <Break height={30} />
         {searchParams?.type === "list" ? (
           <Flex className={styles.dormitoriesList}>
-            {asc(dormitoriesWithPreviews, FIELD_ID)?.map((dormitoriesWithPreview: Dormitory, i: number) => (
-              <DormitoryList {...dormitoriesWithPreview} />
+            {filteredDormitoriesWithPreviews?.map((filteredDormitoriesWithPreviews: Dormitory, i: number) => (
+              <DormitoryList {...filteredDormitoriesWithPreviews} key={i} />
             ))}
           </Flex>
         ) : (
           <Grid className={styles.dormitories}>
-            {asc(dormitoriesWithPreviews, FIELD_ID)?.map((dormitoriesWithPreview: Dormitory, i: number) => (
-              <DormitoryCard {...dormitoriesWithPreview} />
+            {filteredDormitoriesWithPreviews?.map((filteredDormitoriesWithPreviews: Dormitory, i: number) => (
+              <DormitoryCard {...filteredDormitoriesWithPreviews} key={i} />
             ))}
           </Grid>
         )}
