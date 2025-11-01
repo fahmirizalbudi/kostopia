83d2832d0a9b98d6fb22dbbab11671bdffb62a52
fahmirizalbudi <fahmirizal.dev@gmail.com>
2025-11-01 08:07:29 +0700

    refactor: add classname props into datepicker component

--- web/src/app/components/forms/DatePicker.tsx
+++ web/src/app/components/forms/DatePicker.tsx
@@ -7,9 +7,10 @@ interface DatePickerProps {
   placeholder?: string
   value: String
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+  className?: string
 }
 
-const DatePicker = ({ placeholder = "Pilih tanggal ...", value, onChange }: DatePickerProps) => {
+const DatePicker = ({ placeholder = "Pilih tanggal ...", value, onChange, className }: DatePickerProps) => {
   const inputRef = useRef<HTMLInputElement>(null)
 
   const handleClick = () => {
@@ -17,7 +18,7 @@ const DatePicker = ({ placeholder = "Pilih tanggal ...", value, onChange }: Date
   }
 
   return (
-    <div className={`${styles.datePicker} ${value ? styles.hasValue : ""}`} onClick={handleClick}>
+    <div className={`${styles.datePicker} ${value ? styles.hasValue : ""} ${className}`} onClick={handleClick}>
       <input type="date" value={String(value)} onChange={onChange} onFocus={(e: React.FocusEvent<HTMLInputElement>) => e.target.blur()} ref={inputRef} />
       <span className={styles.placeholder}>{value || placeholder}</span>
     </div>
