2af71561246923fbf899bdc4c21e112024a8648c
fahmirizalbudi <fahmirizal.dev@gmail.com>
2025-11-11 04:30:33 +0700

    feat: add placement props to tooltip default on top

--- web/src/app/components/ui/Tooltip.tsx
+++ web/src/app/components/ui/Tooltip.tsx
@@ -7,10 +7,11 @@ import { ReactElement } from "react"
 type TooltipProps = {
   children: ReactElement
   text: string
+  placement?: "top" | "bottom" | "left" | "right"
 }
 
-const Tooltip = ({ children, text }: TooltipProps) => {
-  return <Tippy content={text}>{children}</Tippy>
+const Tooltip = ({ children, text, placement = "top" }: TooltipProps) => {
+  return <Tippy content={text} placement={placement}>{children}</Tippy>
 }
 
 export default Tooltip
